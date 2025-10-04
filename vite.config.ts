39c952ae2d302615ce6ab4348ecc8b3df26533e7
Mateusz Woźniak <matisiek11@gmail.com>
2025-10-04 15:01:35 +0200

    ts build

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import {resolve} from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(),],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "ImageAnnotator",
-      fileName: "image-annotator",
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
+    plugins: [vue(), tailwindcss()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "ImageAnnotator",
+            fileName: "image-annotator",
         },
-      },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
     },
-  },
 })
